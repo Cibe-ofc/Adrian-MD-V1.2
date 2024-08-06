d6132b82ac751247f6c11286c1d3790b6b4c20ad
Cibe-ofc <113320411+Cibe-ofc@users.noreply.github.com>
2024-08-06 22:33:03 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['66958716302']
+global.botname = 'POCONGSTARBOY DARK/DARK'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "POCONGSTARBOY"
+global.sticker2 = "😈"
